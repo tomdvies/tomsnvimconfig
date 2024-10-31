7b27c4760eba01bf25a66a3ccbe59f2558153ec5
Tom <davies.evan.tom@gmail.com>
2024-10-31 11:08:02 +0000

    buble line

--- lua/plugins/line.lua
+++ lua/plugins/line.lua
@@ -3,23 +3,52 @@ return {
         'nvim-lualine/lualine.nvim',
         dependencies = {'nvim-tree/nvim-web-devicons', 'folke/noice.nvim'},
         config = function()
+            local colors = {
+                blue = '#80a0ff',
+                cyan = '#79dac8',
+                black = '#080808',
+                white = '#c6c6c6',
+                red = '#ff5189',
+                violet = '#d183e8',
+                grey = '#303030'
+            }
+
+            local bubbles_theme = {
+                normal = {
+                    a = {fg = colors.black, bg = colors.violet},
+                    b = {fg = colors.white, bg = colors.grey},
+                    c = {fg = colors.white}
+                },
+
+                insert = {a = {fg = colors.black, bg = colors.blue}},
+                visual = {a = {fg = colors.black, bg = colors.cyan}},
+                replace = {a = {fg = colors.black, bg = colors.red}},
+
+                inactive = {
+                    a = {fg = colors.white, bg = colors.black},
+                    b = {fg = colors.white, bg = colors.black},
+                    c = {fg = colors.white}
+                }
+            }
+
             require('lualine').setup({
                 options = {
                     icons_enabled = true,
-                    theme = 'auto',
-                    component_separators = {left = '', right = ''},
-                    section_separators = {left = '', right = ''},
-                    disabled_filetypes = {
-                        statusline = {},
-                        winbar = {},
-                    },
+                    theme = bubbles_theme,
+                    component_separators = '', -- {left = '', right = ''},
+                    section_separators = {left = '', right = ''},
+                    disabled_filetypes = {statusline = {}, winbar = {}},
                     ignore_focus = {},
                     always_divide_middle = true,
                     globalstatus = false,
                     refresh = {statusline = 1000, tabline = 1000, winbar = 1000}
                 },
                 sections = {
-                    lualine_a = {'mode'},
+                    lualine_a = {{
+                        'mode',
+                        separator = {left = ''},
+                        right_padding = 2
+                    }},
                     lualine_b = {'branch', 'diff', 'diagnostics'},
                     lualine_c = {'filename'},
                     lualine_x = {
@@ -30,7 +59,11 @@ return {
                         }
                     },
                     lualine_y = {'progress'},
-                    lualine_z = {'location'}
+                    lualine_z = {{
+                        'location',
+                        separator = {right = ''},
+                        left_padding = 2
+                    }}
                 },
                 inactive_sections = {
                     lualine_a = {},
